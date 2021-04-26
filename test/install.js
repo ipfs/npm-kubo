4c07d7c54aed416829cdc319988ab71d53e6f80f
Marcin Rataj <lidel@lidel.org>
2021-04-26 19:41:34 +0200

    feat: cache and verify downloaded archive (#32)
    * feat: cache and verify downloaded archive
    
    - replaces node-fetch with got which simplifies code
      and is more robust on CI (retries twice before failing to download)
    - adds caching of downloaded archives which should speed up CI
      of projects using this library (eg. ipfs-desktop)
    - adds pre-commit hook to protect against commiting real binary to git
    - downloads .sha512 manifest and compares it with sha512sum of
      downloaded archive
    
    * test: ensure fetched version match dep version
    * test(ci): run npm test on ci

--- test/install.js
+++ test/install.js
@@ -2,31 +2,43 @@ const fs = require('fs-extra')
 const path = require('path')
 const test = require('tape')
 const execa = require('execa')
+const cachedir = require('cachedir')
 
 /*
-  Test that go-ipfs is downloaded during npm install.
-  - package up the current source code with `npm pack`
-  - install the tarball into the example project
-  - ensure that the "go-ipfs.version" prop in the package.json is used
+  Test that correct go-ipfs is downloaded during npm install.
 */
 
-const testVersion = require('./fixtures/example-project/package.json')['go-ipfs'].version
+const expectedVersion = require('../package.json').version
 
 async function clean () {
   await fs.remove(path.join(__dirname, 'fixtures', 'example-project', 'node_modules'))
   await fs.remove(path.join(__dirname, 'fixtures', 'example-project', 'package-lock.json'))
+  await fs.remove(cachedir('npm-go-ipfs'))
 }
 
 test.onFinish(clean)
 
-test('Ensure go-ipfs.version defined in parent package.json is used', async (t) => {
+test('Ensure go-ipfs defined in package.json is fetched on dependency install', async (t) => {
   await clean()
+
+  const exampleProjectRoot = path.join(__dirname, 'fixtures', 'example-project')
 
   // from `example-project`, install the module
   const res = execa.sync('npm', ['install'], {
-    cwd: path.join(__dirname, 'fixtures', 'example-project')
+    cwd: exampleProjectRoot
   })
-  const msg = `Downloading https://dist.ipfs.io/go-ipfs/${testVersion}`
-  t.ok(res.stdout.includes(msg), msg)
+
+  // confirm package.json is correct
+  const fetchedVersion = require(path.join(exampleProjectRoot, 'node_modules', 'go-ipfs', 'package.json')).version
+  t.ok(expectedVersion === fetchedVersion, `package.json versions match '${expectedVersion}'`)
+
+  // confirm binary is correct
+  const binary = path.join(exampleProjectRoot, 'node_modules', 'go-ipfs', 'bin', 'ipfs')
+  const versionRes = execa.sync(binary, ['--version'], {
+    cwd: exampleProjectRoot
+  })
+
+  t.ok(versionRes.stdout === `ipfs version ${expectedVersion}`, `ipfs --version output match '${expectedVersion}'`)
+
   t.end()
 })
