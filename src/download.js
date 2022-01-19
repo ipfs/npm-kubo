8e01f3ccab9aa4012c0c781badf2fda603f9f6e1
Alex Potsides <alex@achingbrain.net>
2022-01-19 16:48:51 +0000

    chore: update src/download.js

--- src/download.js
+++ src/download.js
@@ -13,7 +13,6 @@
     go-ipfs architecture: the architecture of the hardware this program is run from
     go-ipfs install path: './go-ipfs'
 */
-// @ts-ignore no types
 const goenv = require('./go-platform')
 const gunzip = require('gunzip-maybe')
 const got = require('got').default
